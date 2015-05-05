d113503e3507333e33dfc35ae0836eb0cb99c3da
JazSingh <jaspreet_singh_@hotmail.com>
2015-05-05 14:38:07 +0200

    swek

--- ST-Project/GameState.cs
+++ ST-Project/GameState.cs
@@ -57,6 +57,16 @@ namespace ST_Project
             p.set_position(i);
         }
 
+        public bool CheckFinished()
+        {
+            if (GetPlayer().get_position() == GetDungeon().nodes.Length - 1)
+            {
+                Console.WriteLine("Reached end node!");
+                return true;
+            }
+            return false;
+        }
+
         private void DropItems()
         {
             int i = 1;
@@ -74,15 +84,5 @@ namespace ST_Project
                 i++;
             }
         }
-        public bool CheckFinished()
-        {
-            if (GetPlayer().get_position() == GetDungeon().nodes.Length - 1)
-            {
-                Console.WriteLine("Reached end node!");
-                return true;
-            }
-            return false;
-        }
-
     }
 }
