74abcf61bd5515472b431dd834321adb6026312f
DStekel3 <danielstekelenburg@hotmail.com>
2015-05-07 20:42:06 +0200

    sgss

---
+++ TestProject/ItemTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ST_Project;
+
+namespace TestProject
+{
+    [TestClass]
+    class ItemTests
+    {
+        ///
+    }
+}
--- TestProject/PackTests.cs
+++ TestProject/PackTests.cs
@@ -1,86 +1,13 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ST_Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TestProject
 {
-    [TestClass]
-    public class MonsterTests
-    {
-        [TestMethod]
-        public void HP_DamageTest()
-        {
-            HPTest();
-            damageTest();
-        }
-
-        [TestMethod]
-        public void GetsHitTest()
-        {
-            gets_hit_alive();
-            get_hit_dead();
-        }
-
-        [TestMethod]
-        public void ToString()
-        {
-            Monster m = new Monster();
-            string s = string.Empty;
-
-            s += "HP: " + 15 + Environment.NewLine;
-            s += "Damage: " + 3;
-
-            string expected = s;
-            string actual = m.ToString();
-            Assert.AreEqual(expected, actual);
-        }
-
-        public void gets_hit_alive()
-        {
-            Monster target = new Monster();
-            
-            bool expected = false;
-            bool actual = target.gets_hit(8);
-            Assert.AreEqual(expected, actual);
-            
-            int exp = 15 - 8;
-            int act = target.GetHP();
-            Assert.AreEqual(exp, act);
-        }
-
-        public void get_hit_dead()
-        {
-            Monster target = new Monster();
-
-            bool expected = false;
-            bool actual = target.gets_hit(8);
-            Assert.AreEqual(expected, actual);
-
-            int exp = 15 - 8;
-            int act = target.GetHP();
-            Assert.AreEqual(exp, act);
-            
-            expected = true;
-            actual = target.gets_hit(8);
-            Assert.AreEqual(expected, actual);
-        }
-
-        public void HPTest()
-        {
-            Monster m = new Monster();
-            int expected = 15;
-            int actual = m.GetHP();
-            Assert.AreEqual(expected, actual);
-        }
-
-        public void damageTest()
-        {
-            Monster target = new Monster();
-            int expected = 3;
-            int actual = target.hits();
-            Assert.AreEqual(expected, actual);
-        }
-    }
 
     [TestClass]
     public class PackTests
@@ -135,7 +62,7 @@ namespace TestProject
 
             p.Add_Monster(new Monster());
 
-            Assert.AreEqual(initial+15, p.getInitialHP());     // check if initial HP is upped
+            Assert.AreEqual(initial + 15, p.getInitialHP());     // check if initial HP is upped
             Assert.AreEqual(amount + 1, p.GetNumMonsters());  // check if #monsters is +1
         }
 
