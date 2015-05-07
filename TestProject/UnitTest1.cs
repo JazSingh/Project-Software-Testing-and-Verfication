173925a85ee4a10ceadaa698e44bafd304f79dc2
DStekel3 <danielstekelenburg@hotmail.com>
2015-05-07 12:23:12 +0200

    MonsterTests-class volledig, begin PackTests

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -5,19 +5,64 @@ using ST_Project;
 namespace TestProject
 {
     [TestClass]
-    public class MonsterTest
+    public class MonsterTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void HP_DamageTest()
         {
             HPTest();
             damageTest();
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void GetsHitTest()
         {
+            gets_hit_alive();
+            get_hit_dead();
+        }
 
+        [TestMethod]
+        public void ToString()
+        {
+            Monster m = new Monster();
+            string s = string.Empty;
+
+            s += "HP: " + 15 + Environment.NewLine;
+            s += "Damage: " + 3;
+
+            string expected = s;
+            string actual = m.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void gets_hit_alive()
+        {
+            Monster target = new Monster();
+            
+            bool expected = false;
+            bool actual = target.gets_hit(8);
+            Assert.AreEqual(expected, actual);
+            
+            int exp = 15 - 8;
+            int act = target.GetHP();
+            Assert.AreEqual(exp, act);
+        }
+
+        public void get_hit_dead()
+        {
+            Monster target = new Monster();
+
+            bool expected = false;
+            bool actual = target.gets_hit(8);
+            Assert.AreEqual(expected, actual);
+
+            int exp = 15 - 8;
+            int act = target.GetHP();
+            Assert.AreEqual(exp, act);
+            
+            expected = true;
+            actual = target.gets_hit(8);
+            Assert.AreEqual(expected, actual);
         }
 
         public void HPTest()
@@ -35,5 +80,11 @@ namespace TestProject
             int actual = target.hits();
             Assert.AreEqual(expected, actual);
         }
+    }
+
+    [TestClass]
+    public class PackTests
+    {
+
     }
 }
