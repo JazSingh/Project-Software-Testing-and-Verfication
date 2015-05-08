06060133ae13fe77fd1d3573e3afc72eed8ccd6f
JazSingh <jaspreet_singh_@hotmail.com>
2015-05-08 13:04:39 +0200

    deel van dungeon tests

--- ST-Project/Dungeon.cs
+++ ST-Project/Dungeon.cs
@@ -108,7 +108,6 @@ namespace ST_Project
                 if(nodes[i] != null)
                     partitionList.Add(i);
 
-            if (partitionList.Count == 0) return;
             int u;
             int v;
             u = partitionList[Oracle.GiveNumber(partitionList.Count-1)];
@@ -167,11 +166,9 @@ namespace ST_Project
             if (n.Retreat())
             {
                 int[] adj = n.getadj();
-                Random r = new Random();
-                int next = adj[r.Next(0, n.NumNeighbours)];
+                int next = adj[Oracle.GiveNumber(0, n.NumNeighbours == 1? 0 : n.NumNeighbours-2)];
                 Pack p = nodes[pos].popPack();
-                while (next == nodes.Length - 1)
-                    next = adj[r.Next(0, n.NumNeighbours)];
+
                 nodes[next].pushPack(p);
                 Console.WriteLine("Pack RETREATS naar node "+next);
                 return true;
