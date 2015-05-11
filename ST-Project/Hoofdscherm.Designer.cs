1acd23fe562a0ea273f53c8a4eb71f48584e9aa1
JazSingh <jaspreet_singh_@hotmail.com>
2015-05-11 13:02:57 +0200

    swek

--- ST-Project/Hoofdscherm.Designer.cs
+++ ST-Project/Hoofdscherm.Designer.cs
@@ -30,17 +30,16 @@
         {
             this.newgame_b = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // newgame_b
             // 
             this.newgame_b.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.newgame_b.Location = new System.Drawing.Point(548, 123);
+            this.newgame_b.Location = new System.Drawing.Point(1096, 237);
+            this.newgame_b.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.newgame_b.Name = "newgame_b";
-            this.newgame_b.Size = new System.Drawing.Size(179, 69);
+            this.newgame_b.Size = new System.Drawing.Size(358, 133);
             this.newgame_b.TabIndex = 0;
             this.newgame_b.Text = "New Game";
             this.newgame_b.UseVisualStyleBackColor = true;
@@ -49,29 +48,21 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(548, 349);
+            this.button2.Location = new System.Drawing.Point(1096, 671);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(179, 69);
+            this.button2.Size = new System.Drawing.Size(358, 133);
             this.button2.TabIndex = 1;
             this.button2.Text = "Load Game";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::ST_Project.Properties.Resources.background;
-            this.pictureBox1.Location = new System.Drawing.Point(71, 57);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(221, 446);
-            this.pictureBox1.TabIndex = 8;
-            this.pictureBox1.TabStop = false;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(885, 40);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(1770, 77);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(152, 50);
+            this.button1.Size = new System.Drawing.Size(304, 96);
             this.button1.TabIndex = 9;
             this.button1.Text = "Highscores";
             this.button1.UseVisualStyleBackColor = true;
@@ -79,20 +70,19 @@
             // 
             // Hoofdscherm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(962, 526);
+            this.ClientSize = new System.Drawing.Size(1924, 1012);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.newgame_b);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Hoofdscherm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hoofdscherm";
             this.Load += new System.EventHandler(this.DungeonRPG_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -102,7 +92,6 @@
 
         private System.Windows.Forms.Button newgame_b;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
     }
 }
