52512b40f2ceb9420fc20770d1cb74d524ede8f8
JazSingh <jaspreet_singh_@hotmail.com>
2015-06-04 17:13:04 +0200

    test5

--- UnitTestProject1/Test5.cs
+++ UnitTestProject1/Test5.cs
@@ -1,16 +1,93 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ST_Project;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class Test5
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
             // “Monster retreats is handled correctly (they retreat after losing 70% HP; 
             // But when they have an order, they should never retreat)”.
+            //>= 30%     - No order, Hunt, Defend
+            //<30%       - No order, Hunt, Defend
+            //<30%       - No order but other neighbours are full
+        
+        [TestMethod]
+        public void DefendG30()
+        {
+            Replayer z = new Replayer("DefG30.txt");
+            z.Init();
+            while (z.HasNext())
+                z.Step();
+            GameState s = z.QueryState();
+            Assert.IsTrue(s.GetDungeon().nodes[s.GetPlayer().get_position()].hasPack());
+        }
+
+        [TestMethod]
+        public void DefendL30()
+        {
+            Replayer z = new Replayer("DefL30.txt");
+            z.Init();
+            while (z.HasNext())
+                z.Step();
+            GameState s = z.QueryState();
+            Assert.IsTrue(s.GetDungeon().nodes[s.GetPlayer().get_position()].hasPack());
+        }
+
+        [TestMethod]
+        public void HuntG30()
+        {
+            Replayer z = new Replayer("HuntG30.txt");
+            z.Init();
+            while (z.HasNext())
+                z.Step();
+            GameState s = z.QueryState();
+            Assert.IsTrue(s.GetDungeon().nodes[s.GetPlayer().get_position()].hasPack());
+        }
+
+        [TestMethod]
+        public void HuntL30()
+        {
+            Replayer z = new Replayer("HuntL30.txt");
+            z.Init();
+            while (z.HasNext())
+                z.Step();
+            GameState s = z.QueryState();
+            Assert.IsTrue(s.GetDungeon().nodes[s.GetPlayer().get_position()].hasPack());
+        }
+
+        [TestMethod]
+        public void NoOrdG30()
+        {
+            Replayer z = new Replayer("NoOrdG30.txt");
+            z.Init();
+            while (z.HasNext())
+                z.Step();
+            GameState s = z.QueryState();
+            Assert.IsTrue(s.GetDungeon().nodes[s.GetPlayer().get_position()].hasPack());
+        }
+
+        [TestMethod]
+        public void NoOrdL30()
+        {
+            Replayer z = new Replayer("NoOrdL30.txt");
+            z.Init();
+            while (z.HasNext())
+                z.Step();
+            GameState s = z.QueryState();
+            Assert.IsFalse(s.GetDungeon().nodes[s.GetPlayer().get_position()].hasPack());
+        }
+
+        [TestMethod]
+        public void NoOrdL30FullNeighs()
+        {
+            Replayer z = new Replayer("NoOrdL30.txt");
+            z.Init();
+            while (z.HasNext())
+                z.Step();
+            GameState s = z.QueryState();
+            Assert.IsTrue(s.GetDungeon().nodes[s.GetPlayer().get_position()].hasPack());
         }
     }
 }
